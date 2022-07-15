2cbb06dde6c56de1d726f4bbcf879bddf8f1e58e
weqy <109391494+weqy@users.noreply.github.com>
2022-07-15 18:02:17 -0400

    Add files via upload

---
+++ numbergame.py
@@ -0,0 +1,12 @@
+a = 8
+b = 10
+c = 5
+
+print("a + b =", a+b)
+print("b = ", "c + c")
+print("c = 5")
+
+answer=float(input("What is A? (Type a Number) : "))
+if answer == a:
+    print("Correct answer!")
+else: print("Incorrect answer")
