9af2ed1ab9cc662a8d216c0143a75e045b39d5e7
weqy <109391494+weqy@users.noreply.github.com>
2022-07-16 18:40:36 -0400

    Add files via upload

--- numbergame.py
+++ numbergame.py
@@ -1,12 +1,16 @@
-a = 8
-b = 10
-c = 5
+import random
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [3, 4, 5, 6, 7, 8]
+
+a = (random.choice(list1))
+c = (random.choice(list2))
+b = c + c
 
 print("a + b =", a+b)
 print("b = ", "c + c")
-print("c = 5")
+print("c =", c)
 
 answer=float(input("What is A? (Type a Number) : "))
 if answer == a:
     print("Correct answer!")
-else: print("Incorrect answer")
+else: print("Incorrect answer. A was", (random.choice(list1)))
